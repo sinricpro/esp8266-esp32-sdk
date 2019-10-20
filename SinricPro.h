9b8cc741e883bee7d2e769d80ad001494cca6867
sivar2311 <boris.jaeger@gmx.net>
2019-10-20 21:12:41 +0200

    added SinricPro.h in root directory to achive arduino library manager compatibility

---
+++ SinricPro.h
@@ -0,0 +1,8 @@
+/*
+ *  Copyright (c) 2019 Sinric. All rights reserved.
+ *  Licensed under Creative Commons Attribution-Share Alike (CC BY-SA)
+ *
+ *  This file is part of the Sinric Pro (https://github.com/sinricpro/)
+ */
+
+#include "src/SinricPro.h"
