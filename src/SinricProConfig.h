4b9b2a01d292d32289001ebaf2bdfb91138c93ec
sivar2311 <boris.jaeger@gmx.net>
2019-11-18 05:04:23 +0100

    removed unused #defines

--- src/SinricProConfig.h
+++ src/SinricProConfig.h
@@ -16,10 +16,6 @@
 #define UDP_MULTICAST_IP IPAddress(224,9,9,9)
 #define UDP_MULTICAST_PORT 3333
 
-// if websocket connection cannot established after WEBSOCKET_CONNECTION_TIMEOUT milliseconds
-// websocket will try to connect again
-#define WEBSOCKET_CONNECTION_TIMEOUT 1000
-
 // websocket sends every WEBSOCKET_PING_INTERVAL milliseconds a ping to Server
 // if there is no pong received after WEBSOCKET_PING_TIMEOUT milliseconds, retry count is incremented by one
 // if retry count reaches WEBSOCKET_RETRY_COUNT websocket connection is closed and try to reconnect server
