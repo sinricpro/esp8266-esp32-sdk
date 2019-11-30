fb74d25269386362caf525aa94d484e769f023dd
sivar2311 <boris.jaeger@gmx.net>
2019-11-30 09:34:44 +0100

    Added Temperaturesensor example

---
+++ pio-examples/temperaturesensor/README.md
@@ -0,0 +1,6 @@
+# GarageDoorExample
+![Wiring](https://github.com/sivar2311/GarageDoorExample/raw/master/GarageDoor3_Steckplatine.png)
+  
+D5: connected to relay  
+D6: connected to reed_switch (OPEN ENDSTOP)  
+D7: connected to reed_switch (CLOSED ENDSTOP)  
---
+++ pio-examples/temperaturesensor/src/temperaturesensor.cpp
@@ -0,0 +1,142 @@
+/*
+ * Example for how to use SinricPro Temperaturesensor device:
+ * - setup a temperature sensor device
+ * - send temperature event to SinricPro server when temperature has changed
+ * 
+ * DHT Library used in this example: https://github.com/markruys/arduino-DHT
+ * DHT Sensor is connected to D5 on ESP8266 devices / GPIO 5 on ESP32 devices
+ * 
+ * If you encounter any issues:
+ * - open serial monitor and check whats happening
+ * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
+ */
+
+#include <Arduino.h>
+#ifdef ESP8266 
+       #include <ESP8266WiFi.h>
+#endif 
+#ifdef ESP32   
+       #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProTemperaturesensor.h"
+#include "DHT.h"
+
+#define WIFI_SSID         "YOUR-WIFI-SSID"    
+#define WIFI_PASS         "YOUR-WIFI-PASSWORD"
+#define APP_KEY           "YOUR-APP-KEY"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "YOUR-APP-SECRET"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define TEMP_SENSOR_ID    "YOUR-DEVICE-ID"    // Should look like "5dc1564130xxxxxxxxxxxxxx"
+#define BAUD_RATE         115200              // Change baudrate to your need (used for serial monitor)
+#define EVENT_WAIT_TIME   60000               // send event every 60 seconds
+
+#ifdef ESP8266
+       #define DHT_PIN    D5
+#endif
+#ifdef ESP32
+       #define DHT_PIN    5
+#endif
+
+DHT dht;                                      // DHT sensor
+
+bool deviceIsOn;                              // Temeprature sensor on/off state
+float temperature;                            // actual temperature
+float humidity;                               // actual humidity
+float lastTemperature;                        // last known temperature (for compare)
+float lastHumidity;                           // last known humidity (for compare)
+unsigned long lastEvent = (-EVENT_WAIT_TIME); // last time event has been sent
+
+/* bool onPowerState(String deviceId, bool &state) 
+ *
+ * Callback for setPowerState request
+ * parameters
+ *  String deviceId (r)
+ *    contains deviceId (useful if this callback used by multiple devices)
+ *  bool &state (r/w)
+ *    contains the requested state (true:on / false:off)
+ *    must return the new state
+ * 
+ * return
+ *  true if request should be marked as handled correctly / false if not
+ */
+bool onPowerState(const String &deviceId, bool &state) {
+  Serial.printf("Temperaturesensor turned %s (via SinricPro) \r\n", state?"on":"off");
+  deviceIsOn = state; // turn on / off temperature sensor
+  return true; // request handled properly
+}
+
+/* handleTemperatatureSensor()
+ * - Checks if Temperaturesensor is turned on
+ * - Checks if time since last event > EVENT_WAIT_TIME to prevent sending too much events
+ * - Get actual temperature and humidity and check if these values are valid
+ * - Compares actual temperature and humidity to last known temperature and humidity
+ * - Send event to SinricPro Server if temperature or humidity changed
+ */
+void handleTemperaturesensor() {
+  if (deviceIsOn == false) return; // device is off...do nothing
+
+  unsigned long actualMillis = millis();
+  if (actualMillis - lastEvent < EVENT_WAIT_TIME) return; //only check every EVENT_WAIT_TIME milliseconds
+
+  temperature = dht.getTemperature();          // get actual temperature
+  humidity = dht.getHumidity();                // get actual humidity
+
+  if (isnan(temperature) || isnan(humidity)) { // reading failed... 
+    Serial.printf("DHT reading failed!\r\n");  // print error message
+    return;                                    // try again next time
+  } 
+
+  if (temperature == lastTemperature || humidity == lastHumidity) return; // if no values changed do nothing...
+
+  SinricProTemperaturesensor &mySensor = SinricPro[TEMP_SENSOR_ID];  // get temperaturesensor device
+  bool success = mySensor.sendTemperatureEvent(temperature, humidity); // send event
+  if (success) {  // if event was sent successfuly, print temperature and humidity to serial
+    Serial.printf("Temperature: %2.1f Celsius\tHumidity: %2.1f%%\r\n", temperature, humidity);
+  } else {  // if sending event failed, print error message
+    Serial.printf("Something went wrong...could not send Event to server!\r\n");
+  }
+
+  lastTemperature = temperature;  // save actual temperature for next compare
+  lastHumidity = humidity;        // save actual humidity for next compare
+  lastEvent = actualMillis;       // save actual time for next compare
+}
+
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+  IPAddress localIP = WiFi.localIP();
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n", localIP[0], localIP[1], localIP[2], localIP[3]);
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add device to SinricPro
+  SinricProTemperaturesensor &mySensor = SinricPro[TEMP_SENSOR_ID];
+  mySensor.onPowerState(onPowerState);
+
+  // setup SinricPro
+  SinricPro.begin(APP_KEY, APP_SECRET);
+  SinricPro.restoreDeviceStates(true); // get latest known deviceState from server (is device turned on?)
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE);
+  dht.setup(DHT_PIN);
+
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+  handleTemperaturesensor();
+}
