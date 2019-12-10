91c0fa917b4f8d49d058528a5b612631ab94a69d
sivar2311 <sivar2311@googlemail.com>
2019-12-10 05:30:14 +0100

    Added example
    Dimmable Switch

---
+++ examples/DimSwitch/DimSwitch.ino
@@ -0,0 +1,99 @@
+/*
+ * If you encounter any issues:
+ * - check the readme.md at https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md
+ * - ensure all dependent libraries are installed
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#arduinoide
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#dependencies
+ * - open serial monitor and check whats happening
+ * - check full user documentation at https://sinricpro.github.io/esp8266-esp32-sdk
+ * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
+ */
+
+// Uncomment the following line to enable serial debug output
+//#define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
+       #define DEBUG_ESP_PORT Serial
+       #define NODEBUG_WEBSOCKETS
+       #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#ifdef ESP8266 
+       #include <ESP8266WiFi.h>
+#endif 
+#ifdef ESP32   
+       #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProDimSwitch.h"
+
+#define WIFI_SSID         "YOUR-WIFI-SSID"    
+#define WIFI_PASS         "YOUR-WIFI-PASSWORD"
+#define APP_KEY           "YOUR-APP-KEY"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "YOUR-APP-SECRET"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define DIMSWITCH_ID      "YOUR-DEVICE-ID"    // Should look like "5dc1564130xxxxxxxxxxxxxx"
+#define BAUD_RATE         9600                // Change baudrate to your need
+
+// we use a struct to store all states and values for our dimmable switch
+struct {
+  bool powerState = false;
+  int powerLevel = 0;
+} device_state;
+
+bool onPowerState(const String &deviceId, bool &state) {
+  Serial.printf("Device %s power turned %s \r\n", deviceId.c_str(), state?"on":"off");
+  device_state.powerState = state;
+  return true; // request handled properly
+}
+
+bool onPowerLevel(const String &deviceId, int &powerLevel) {
+  device_state.powerLevel = powerLevel;
+  Serial.printf("Device %s power level changed to %d\r\n", deviceId.c_str(), device_state.powerLevel);
+  return true;
+}
+
+bool onAdjustPowerLevel(const String &deviceId, int levelDelta) {
+  device_state.powerLevel += levelDelta;
+  Serial.printf("Device %s power level changed about %i to %d\r\n", deviceId.c_str(), levelDelta, device_state.powerLevel);
+  levelDelta = device_state.powerLevel;
+  return true;
+}
+
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+  IPAddress localIP = WiFi.localIP();
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n", localIP[0], localIP[1], localIP[2], localIP[3]);
+}
+
+void setupSinricPro() {
+  SinricProDimSwitch &myDimSwitch = SinricPro[DIMSWITCH_ID];
+
+  // set callback function to device
+  myDimSwitch.onPowerState(onPowerState);
+  myDimSwitch.onPowerLevel(onPowerLevel);
+  myDimSwitch.onAdjustPowerLevel(onAdjustPowerLevel);
+
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE);
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+}
