c102bf5fdc44634e42e76af34907a426a092c02d
sivar2311 <sivar2311@googlemail.com>
2020-08-12 08:31:48 +0200

    Documentation Update

--- docs/search/typedefs_4.js
+++ docs/search/typedefs_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['increasecolortemperaturecallback_209',['IncreaseColorTemperatureCallback',['../class_sinric_pro_light.html#ac29cb175180abefcc181191de6be6c9f',1,'SinricProLight']]]
+  ['increasecolortemperaturecallback_215',['IncreaseColorTemperatureCallback',['../class_sinric_pro_light.html#ac29cb175180abefcc181191de6be6c9f',1,'SinricProLight']]]
 ];
