d4ddd9cfabc4f09af282ca6dd45dc797b9e99800
sivar2311 <sivar2311@googlemail.com>
2021-03-09 15:22:22 +0100

    Removed external crypto lib - use ESP8266 / ESP32 core libs

--- src/SinricProSignature.h
+++ src/SinricProSignature.h
@@ -8,31 +8,56 @@
 #ifndef _SIGNATURE_H_
 #define _SIGNATURE_H_
 
-#include "extralib/Crypto/Crypto.h"
-#include "extralib/Crypto/Base64.h"
+#if defined (ESP8266)
+  #include <bearssl/bearssl_hmac.h>
+  #include <base64.h>
+#endif
+#if defined (ESP32)
+  #include "mbedtls/md.h"
+  #include "mbedtls/base64.h"
+#endif
+
+
+String HMACbase64(const String &message, const String &key) {
+  byte hmacResult[32];
+
+#if defined(ESP8266)
+  br_hmac_key_context keyContext; // Holds general HMAC info
+  br_hmac_context hmacContext;    // Holds general HMAC info + specific info for the current operation
+
+  br_hmac_key_init(&keyContext, &br_sha256_vtable, key.c_str(), key.length());
+  br_hmac_init(&hmacContext, &keyContext, 32);
+  br_hmac_update(&hmacContext, message.c_str(), message.length());
+  br_hmac_out(&hmacContext, hmacResult);
+
+  base64 encoded;
+  return encoded.encode((const uint8_t *)hmacResult, 32, false);
+#endif
+
+#if defined(ESP32)
+  mbedtls_md_context_t ctx;
+  mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;
+
+  mbedtls_md_init(&ctx);
+  mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 1);
+  mbedtls_md_hmac_starts(&ctx, (const unsigned char*) key.c_str(), key.length());
+  mbedtls_md_hmac_update(&ctx, (const unsigned char*) message.c_str(), message.length());
+  mbedtls_md_hmac_finish(&ctx, hmacResult);
+  mbedtls_md_free(&ctx);
+
+  unsigned char encoded[100];
+  size_t written;
+  mbedtls_base64_encode(encoded, 100, &written, (const unsigned char *) encoded, 100);
+  return String{ (char*) encoded};
+
+#endif
+}
 
 String calculateSignature(const char* key, JsonDocument &jsonMessage) {
   if (!jsonMessage.containsKey("payload")) return String("");
   String jsonPayload; serializeJson(jsonMessage["payload"], jsonPayload);
-
-  byte rawSigBuf[SHA256HMAC_SIZE];
-  String test;
-  
-
-  SHA256HMAC hmac((byte*) key, strlen(key));
-  hmac.doUpdate(jsonPayload.c_str(), jsonPayload.length());
-  hmac.doFinal(rawSigBuf);
-  
-
-  int b64_len = base64_enc_len(SHA256HMAC_SIZE);
-  char sigBuf[b64_len+1];
-  base64_encode(sigBuf, (char*) rawSigBuf, SHA256HMAC_SIZE);
-  sigBuf[b64_len] = 0;
-  String result = sigBuf;
-
-  return result;
+  return HMACbase64(jsonPayload, String(key));
 }
-
 bool verifyMessage(String key, JsonDocument &jsonMessage) {
   String jsonHash = jsonMessage["signature"]["HMAC"];
   String calculatedHash = calculateSignature(key.c_str(), jsonMessage);
