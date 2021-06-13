1d044a11765d9540d0d8efff7494ccfc60bfd00d
sivar2311 <sivar2311@googlemail.com>
2021-06-13 15:56:23 +0200

    Implemented fail counter and additional delay

--- src/EventLimiter.h
+++ src/EventLimiter.h
@@ -10,24 +10,42 @@ class EventLimiter {
     operator bool();
   private:
     unsigned long minimum_distance;
-    unsigned long last_event;
+    unsigned long next_event;
+    unsigned long extra_distance;
+    unsigned long fail_counter;
 };
 
 EventLimiter::EventLimiter(unsigned long minimum_distance) 
 : minimum_distance(minimum_distance)
-, last_event(0) {}
+, next_event(0)
+, extra_distance(0)
+, fail_counter(0) {}
 
 EventLimiter::operator bool() {
   unsigned long current_millis = millis();
-  if (last_event) {
-    if (current_millis - last_event >= minimum_distance) {
-      last_event = current_millis;
-      return false;
-    } 
-  } else {
-    last_event = current_millis;
+  unsigned long fail_threshold = (minimum_distance / 4);
+
+  if ( current_millis >= next_event ) {
+
+    if ( fail_counter > fail_threshold ) {
+
+      extra_distance += minimum_distance;
+      fail_counter = 0;
+
+    } else {
+
+      extra_distance = 0;
+
+    }
+
+    next_event = current_millis + minimum_distance + extra_distance;
     return false;
+
   }
+
+  fail_counter++;
+  if (fail_counter == fail_threshold) Serial.printf("WARNING: YOUR CODE SENDS EXCESSIVE EVENTS! EVENTS ARE NOW LIMITED BY AN ADDITIONAL DELAY OF %lu SECONDS. PLEASE CHECK YOUR CODE!\r\n", extra_distance / 1000);
+
   return true;
 }
 
