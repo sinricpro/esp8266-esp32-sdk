d46822d5ca39985cd1a6f32b023e557573979a0c
sivar2311 <sivar2311@googlemail.com>
2022-01-27 16:07:00 +0100

    changed to class and added constructor for easier handling

--- src/SinricProRequest.h
+++ src/SinricProRequest.h
@@ -8,12 +8,20 @@
 #include "SinricProNamespace.h"
 namespace SINRICPRO_NAMESPACE {
 
-struct SinricProRequest {
+class SinricProRequest {
+  public:
+    SinricProRequest(const String &action, const String &instance, JsonObject &request_value, JsonObject &response_value);
     const String &action;
     const String &instance;
     JsonObject &  request_value;
     JsonObject &  response_value;
 };
+
+SinricProRequest::SinricProRequest(const String &action, const String &instance, JsonObject &request_value, JsonObject &response_value)
+    : action(action)
+    , instance(instance)
+    , request_value(request_value)
+    , response_value(response_value) {}
 
 /**
  * @brief Abstract base class inherited by all capability classes that can handle requests.
@@ -24,7 +32,7 @@ class SinricProRequestHandler {
     friend class SinricProDevice;
 
   protected:
-    virtual bool handleRequest(SinricProRequest& request) = 0;
+    virtual bool handleRequest(SinricProRequest &request) = 0;
 };
 
 }  // namespace SINRICPRO_NAMESPACE
