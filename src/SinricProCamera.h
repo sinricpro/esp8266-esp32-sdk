f44b034db07e3601a2d83bef8c51d8cc090cb7de
sivar2311 <sivar2311@gmail.com>
2024-11-25 05:31:21 +0100

    Implemented CameraController

--- src/SinricProCamera.h
+++ src/SinricProCamera.h
@@ -11,6 +11,7 @@
 #include "Capabilities/SettingController.h"
 #include "Capabilities/PushNotification.h"
 #include "Capabilities/PowerStateController.h"
+#include "Capabilities/CameraController.h"
 
 #include "SinricProNamespace.h"
 namespace SINRICPRO_NAMESPACE {
@@ -23,10 +24,12 @@ namespace SINRICPRO_NAMESPACE {
 class SinricProCamera : public SinricProDevice,
                         public SettingController<SinricProCamera>,
                         public PushNotification<SinricProCamera>,
-                        public PowerStateController<SinricProCamera> {
+                        public PowerStateController<SinricProCamera>,
+                        public CameraController<SinricProCamera> {
                         friend class SettingController<SinricProCamera>;
                         friend class PushNotification<SinricProCamera>;
                         friend class PowerStateController<SinricProCamera>;
+                        friend class CameraController<SinricProCamera>;
   public:
 	  SinricProCamera(const String &deviceId) : SinricProDevice(deviceId, "CAMERA") {}
 };
